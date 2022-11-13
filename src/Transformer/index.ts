8fd2bfb796ac2bbdcfdc94b9fd07eb204bedf0f5
Aloento <11802769+Aloento@users.noreply.github.com>
2022-11-13 23:59:57 +0100

    move

--- src/Transformer/index.ts
+++ src/Transformer/index.ts
@@ -4,16 +4,15 @@ import { TransformNon } from "./Non";
 import { TransformOne } from "./One";
 import { TransformTwo } from "./Two";
 
-/** 
+/**
  * Number to LittleEndian HexString
  */
 export function To2LE(num: number): AST[] {
   const hex = num.toString(16);
-  return [hex.slice(2, 4), hex.slice(0, 2)]
-    .map(x => ({
-      type: ASTType.NumberLiteral,
-      value: x,
-    }));
+  return [hex.slice(2, 4), hex.slice(0, 2)].map((x) => ({
+    type: ASTType.NumberLiteral,
+    value: x,
+  }));
 }
 
 /**
@@ -36,7 +35,7 @@ export function Transformer(ast: AST) {
         type: ASTType.CallExpression,
         name: node.name,
         params: [],
-      }
+      };
 
       switch (node.params?.length) {
         case 1:
@@ -53,11 +52,13 @@ export function Transformer(ast: AST) {
           break;
 
         default:
-          throw new Error(`Params length out of range: ${node.params?.length} for opcode: ${node.name}`);
+          throw new Error(
+            `Params length out of range: ${node.params?.length} for opcode: ${node.name}`
+          );
       }
 
       parent!.context!.push(call);
-    }
+    },
   });
 
   return newAst;
--- src/main.tsx
+++ src/main.tsx
@@ -1,5 +1,28 @@
-import { render } from 'preact'
-import { App } from './app'
-import './index.css'
+import { render } from "preact";
+import "./index.css";
+import preactLogo from "./assets/preact.svg";
+import "./app.css";
+import { Tokenizer } from "./Tokenizer";
+import { Parser } from "./Parser";
+import { TestASM } from "./TestASM";
 
-render(<App />, document.getElementById('app') as HTMLElement)
+function App() {
+  // @ts-expect-error
+  window.Tokenizer = Tokenizer;
+  // @ts-expect-error
+  window.Parser = Parser;
+
+  const t = Tokenizer(TestASM);
+  console.log(t);
+
+  const ast = Parser(t);
+  console.log(ast);
+
+  return (
+    <div>
+      <img src={preactLogo} alt="Preact Logo" />
+    </div>
+  );
+}
+
+render(<App />, document.getElementById("app") as HTMLElement);
