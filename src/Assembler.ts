411247f72d2b8dc94038ed283e3737dfc7246799
Aloento <11802769+Aloento@users.noreply.github.com>
2022-11-15 14:09:28 +0100

    imp

--- src/Assembler.ts
+++ src/Assembler.ts
@@ -1,3 +1,8 @@
+import { Generator } from "./Generator";
+import { Parser } from "./Parser";
+import { Tokenizer } from "./Tokenizer";
+import { Transformer } from "./Transformer";
+
 export function Assembler(asm: string): string {
   return Generator(Transformer(Parser(Tokenizer(asm))));
 }
