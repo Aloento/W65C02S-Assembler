7e3fc297207a3d91a8312e03608f0afaaecd6dbe
Aloento <11802769+Aloento@users.noreply.github.com>
2022-11-22 23:47:57 +0100

    TransformPUSH

--- src/Transformer/One.ts
+++ src/Transformer/One.ts
@@ -35,8 +35,42 @@ export function TransformOne(node: AST, call: AST) {
       TransformBCC(arg, call);
       break;
 
+    case OpCode.PUSH:
+      TransformPUSH(arg, call);
+      break;
+
     default:
       throw new Error(`UnSupport OpCode: ${node.name} with 1 param`);
+  }
+}
+
+function TransformPUSH(arg: AST, call: AST) {
+  switch (arg.type) {
+    case ASTType.RegisterLiteral:
+      switch (arg.name) {
+        case Register.Accumulator:
+          call.value = "48";
+          break;
+
+        case Register.IndexX:
+          call.value = "DA";
+          break;
+
+        case Register.IndexY:
+          call.value = "5A";
+          break;
+
+        case Register.StatusRegister:
+          call.value = "28";
+          break;
+
+        default:
+          throw new Error(`UnSupport Register: ${arg.value} for ${OpCode.PUSH}`);
+      }
+      break;
+
+    default:
+      throw new Error(`Unexpected param type: ${arg.type} for ${OpCode.PUSH}`);
   }
 }
 
